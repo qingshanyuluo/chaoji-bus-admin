ae59ecc4a6cb8e19cfd01b0b4940e49b3d3c2024
longlong A dog <qingshanyuluo@gmail.com>
2020-12-23 15:08:43 +0800

    add  interceptors

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-// import { Message } from "element-ui";
+import { Message } from "element-ui";
 import store from "@/store";
 // import BASE_URL from "@/constant";
 
@@ -15,7 +15,7 @@ service.interceptors.request.use(
   request => {
     // 有 token就带上
     if (store.state.isLogin) {
-      request.headers["token"] = store.state.Authorization;
+      request.headers["token"] = store.state.token;
     }
     return request;
   },
@@ -25,45 +25,45 @@ service.interceptors.request.use(
 );
 
 // response拦截器
-// service.interceptors.response.use(
-//   response => {
-//     /**
-//      * code为非200是抛错 可结合自己业务进行修改
-//      */
-//     const res = response.data;
-//     //console.log(res.code);
-//     // console.log(response);
-//     if (res.code !== 200) {
-//       if (res.status) {
-//         return response;
-//       }
-//       if (res.code === 401) {
-//         alert("注册信息已过期，请重新登录！");
-//         window.location = "/login";
-//         return response;
-//         //} else if (res.code === 500) {
-//       } else if (res.status === 500) {
-//         return response;
-//         // Message({
-//         //   message: res.msg,
-//         //   type: "error",
-//         //   duration: 5 * 1000
-//         // });
-//         // return Promise.reject(new Error(res.msg));
-//       }
-//     } else {
-//       return response;
-//     }
-//   },
-//   error => {
-//     console.log("err" + error); // for debug
-//     Message({
-//       message: error.message,
-//       type: "error",
-//       duration: 5 * 1000
-//     });
-//     return Promise.reject(error);
-//   }
-// );
+service.interceptors.response.use(
+  response => {
+    /**
+     * code为非200是抛错 可结合自己业务进行修改
+     */
+    const res = response.data;
+    //console.log(res.code);
+    // console.log(response);
+    if (res.code !== 200) {
+      if (res.status) {
+        return response;
+      }
+      if (res.code === 401) {
+        alert("注册信息已过期，请重新登录！");
+        window.location = "/login";
+        return response;
+        //} else if (res.code === 500) {
+      } else if (res.status === 500) {
+        return response;
+        // Message({
+        //   message: res.msg,
+        //   type: "error",
+        //   duration: 5 * 1000
+        // });
+        // return Promise.reject(new Error(res.msg));
+      }
+    } else {
+      return response;
+    }
+  },
+  error => {
+    console.log("err" + error); // for debug
+    Message({
+      message: error.message,
+      type: "error",
+      duration: 5 * 1000
+    });
+    return Promise.reject(error);
+  }
+);
 
 export default service;
